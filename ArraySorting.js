01e9aa66eab3f10540dc047f6a82b88c86635b53
Nicholas Reeder <nickreeder32@gmail.com>
2021-06-25 13:51:18 -0700

    array sorting solution

---
+++ ArraySorting.js
@@ -0,0 +1,39 @@
+// Write an algorithm that sorts an array without using the sort() method.
+// There are many different sorting algorithms - take the time to read about the following:
+//Example
+
+// Input: [9, 2, 7, 12]
+
+// Output: [2, 7, 9, 12]
+
+function merge(left, right) {
+  let arr = [];
+  // Break out of loop if any one of the array gets empty
+  while (left.length && right.length) {
+    // Pick the smaller among the smallest element of left and right sub arrays
+    if (left[0] < right[0]) {
+      arr.push(left.shift());
+    } else {
+      arr.push(right.shift());
+    }
+  }
+
+  // Concatenating the leftover elements
+  // (in case we didn't go through the entire left or right array)
+  return [...arr, ...left, ...right];
+}
+
+function mergeSort(array) {
+  const half = array.length / 2;
+
+  // Base case or terminating case
+  if (array.length < 2) {
+    return array;
+  }
+
+  const left = array.splice(0, half);
+  return merge(mergeSort(left), mergeSort(array));
+}
+
+array = [4, 8, 7, 2, 11, 1, 3];
+console.log(mergeSort(array));
